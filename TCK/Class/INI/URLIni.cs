2b975673c9f88871ef40c8e87830dae881195d63
jikjky gmail.com <jikjky@gmail.com>
2023-06-26 20:22:11 +0900

    병합

--- TCK/Class/INI/URLIni.cs
+++ TCK/Class/INI/URLIni.cs
@@ -12,7 +12,7 @@ namespace TCK.Class.INI
         public string TwipURL { get; set; } = "";
         public int TwipDelayTime { get; set; } = 8000;
         public string ToonationURL { get; set; } = "";
-        public int ToonationDelayTime { get; set; } = 0;
+        public int ToonationDelayTime { get; set; } = 10000;
         public URLIni(FileInfo fileInfo) : base(fileInfo)
         {
    
